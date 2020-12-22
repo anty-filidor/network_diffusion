dafda84b72021274f0d930254a70421bd2c57098
anty-filidor <michal.czuba.1995@gmail.com>
2020-12-22 16:06:45 +0100

    pep standardisation flat_sperading, utils

--- network_diffusion/utils.py
+++ network_diffusion/utils.py
@@ -1,64 +1,64 @@
+import os
 import string
-import os.path
-from os import path
+from typing import Any, Dict, List
 
 
-def read_mlx(file_path):
+# TODO - json
+def read_mlx(file_path: str) -> Dict[str, List[Any]]:
     """
-    This function reads multilayer network from mlx file to the dictionary of arrays which is then readed by multilayer
-    network class
+    Handler of MLX files for the MultilayerNetwork class.
 
     :param file_path: path to file
+
     :return: a dictionary with network to create class
     """
-
     # initialise empty containers
     net_dict = {}
-    tab = []
-    name = 'foo'
+    tab: List[Any] = []
+    name = "foo"
 
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         line = file.readline()
 
         # omit trash
-        while line and line[0] is not '#':
+        while line and line[0] != "#":
             line = file.readline()
 
         # read pure data
         while line:
             # if line contains title of new division
-            if line[0] is '#':
+            if line[0] == "#":
                 # if this is a special line - type
-                if '#TYPE'.lower() in line.lower():
-                    net_dict.update({'type': [line[6:-1]]})  # '6:-1' to save the name of type
+                if "#TYPE".lower() in line.lower():
+                    net_dict.update(
+                        {"type": [line[6:-1]]}
+                    )  # '6:-1' to save the name of type
                 # else if it is a normal division
                 else:
                     net_dict.update({name: tab})
                     name = line[1:-1].lower()  # omitting '#' and '\n'
                     tab = []
             line = file.readline()
-            # don't save line with only whitespaces or if line contains a title of new division
-            if not line.isspace() and '#' not in line:
-                line = line.translate({ord(char): None for char in string.whitespace})
-                tab.append(line.split(','))
+            # don't save line with only whitespaces or if line contains a
+            # title of new division
+            if not line.isspace() and "#" not in line:
+                line = line.translate(
+                    {ord(char): None for char in string.whitespace}
+                )
+                tab.append(line.split(","))
 
         # append last line to dictionary
         net_dict.update({name: tab[:-1]})
-    del net_dict['foo']
+    del net_dict["foo"]
 
     return net_dict
 
 
-def create_directory(dest_path):
+def create_directory(dest_path: str) -> None:
     """
-    Method checks out if given directory exists and if doesn't it just creates it
+    Checks out if given directory exists and if doesn't it creates it.
 
-    :param dest_path: (str) absolute path to create folder
-    :return: (int) 0 if directory has been created, 1 if directory had been existed before method call
+    :param dest_path: absolute path to create folder
     """
-
-    if path.exists(dest_path):
-        return 1
-    else:
+    if not os.path.exists(dest_path):
         os.mkdir(dest_path)
-        return 0
