8d6b238392f5ffa9ed96092b3116c041e552b9d7
Michał Czuba <mc@vestigit.pl>
2022-08-12 14:27:19 +0200

    added building for PyPI

--- deployment.sh
+++ deployment.sh
@@ -8,7 +8,7 @@ echo 'anty-filidor' | twine upload  dist/*
 mkdir conda_build
 cd conda_build
 conda skeleton pypi network_diffusion
-conda-build network_diffusion
+conda-build network_diffusion --output-folder .
 
 conda convert -f --platform all /usr/local/anaconda3/conda-bld/osx-64/network_diffusion-0.5.29-py37_0.tar.bz2
 mkdir osx-64
@@ -18,9 +18,6 @@ cp /usr/local/anaconda3/conda-bld/osx-64/network_diffusion-0.5.29-py37_0.tar.bz2
 anaconda upload linux-64/network_diffusion-0.5.29-py37_0.tar.bz2
 anaconda upload osx-64/network_diffusion-0.5.29-py37_0.tar.bz2
 anaconda upload win-64/network_diffusion-0.5.29-py37_0.tar.bz2
-
-
-
 
 # https://packaging.python.org/tutorials/packaging-projects/
 # https://docs.conda.io/projects/conda-build/en/latest/user-guide/tutorials/build-pkgs-skeleton.html
