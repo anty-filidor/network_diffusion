fdda0f54c554143b670fb2d8b521911de47ba2c9
Michał Czuba <mc@vestigit.pl>
2022-08-12 14:30:27 +0200

    added building for PyPI

--- deployment.sh
+++ deployment.sh
@@ -21,3 +21,19 @@ anaconda upload win-64/network_diffusion-0.5.29-py37_0.tar.bz2
 
 # https://packaging.python.org/tutorials/packaging-projects/
 # https://docs.conda.io/projects/conda-build/en/latest/user-guide/tutorials/build-pkgs-skeleton.html
+
+
+add_tag:
+  stage: publish
+  before_script:
+    - git config --global user.name "${GITLAB_USER_NAME}"
+    - git config --global user.email "${GITLAB_USER_EMAIL}"
+    - git remote rm origin
+    - git remote add origin
+      https://oauth2:${AUTH_TOKEN}@gitlab.com/${CI_PROJECT_PATH}
+  script:
+    - VER=$(cat nn_inference/__init__.py | grep __version__ | cut -d'"' -f2)
+    - git tag -a ${VER} -m "Version ${VER}"
+    - git push origin --tags
+  rules:
+    - if: $CI_COMMIT_BRANCH == "master"
