ea1c665ee6ed054e1299a707c5af6b10378cd0d4
Mateusz Nurek <mateusz.nurek@pwr.edu.pl>
2023-09-05 13:31:33 +0200

    Add integration with TemporalNetwork class

--- network_diffusion/tpn/tpnetwork.py
+++ network_diffusion/tpn/tpnetwork.py
@@ -28,6 +28,7 @@ from network_diffusion.mln.actor import MLNetworkActor
 from network_diffusion.mln.mlnetwork import MultilayerNetwork
 from network_diffusion.utils import read_tpn
 
+from network_diffusion.tpn.cogsnet_lib import cogsnet
 
 class TemporalNetwork:
     """Container for a temporal network."""
@@ -115,3 +116,34 @@ class TemporalNetwork:
     def get_actors_num(self) -> int:
         """Get number of actors that live in the network."""
         return len(self.get_actors_from_snap(0))
+    
+    @classmethod
+    def from_cogsnet(
+        cls,
+        forgetting_type: str,
+        snapshot_interval: int,
+        edge_lifetime: int,
+        mu: float,
+        theta: float,
+        units: int,
+        path_events: str,
+        delimiter: str
+    ) -> "TemporalNetwork":
+        """
+        Load a temporal network from a csv file with events and create CogSNet with the given parameters.
+
+        Note, the csv file should be in the form of: SRC DST TIME.
+        The timestamps TIME should be in ascending order.
+        The timestamps are expected to be provided in seconds.
+
+        :param str forgetting_type: The forgetting function used to decrease the weight of edges over time. Allowed values are 'exponential', 'power', or 'linear'.
+        :param int snapshot_interval: The interval for taking snapshots (0 or larger) expressed in units param (seconds, minutes, hours). A value of 0 means taking a snapshot after each event.
+        :param int edge_lifetime: The lifetime of an edge after which the edge will disappear if no new event occurs (greater than 0).
+        :param float mu: The value of increasing the weight of an edge for each new event (greater than 0 and less than or equal to 1).
+        :param float theta: The cutoff point (between 0 and mu). If the weight falls below theta, the edge will disappear.
+        :param int units: The time units (1 for seconds, 60 for minutes, or 3600 for hours). For the power forgetting function, this parameter also determines the minimum interval between events to prevent them from being skipped when calculating the weight.
+        :param str path_events: The path to the CSV file with events.
+        :param str delimiter: The delimiter for the CSV file (allowed values are ',', ';', or '\\t').
+        """
+        snaps = cogsnet(forgetting_type, snapshot_interval, edge_lifetime, mu, theta, units, path_events, delimiter)
+        return cls(snaps)
