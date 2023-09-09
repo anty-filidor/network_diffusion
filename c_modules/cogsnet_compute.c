e9e3513259d1debcea2d7d008fc437356aac9485
Michał Czuba <michal.czuba.1995@gmail.com>
2023-09-09 10:04:21 +0200

    implemented multiplatform CICD pipeline

--- c_modules/cogsnet_compute.c
+++ c_modules/cogsnet_compute.c
@@ -17,7 +17,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <unistd.h>
+#include <unistd.h>  // TODO (MN): this is Unix-specific library!
 
 // linear forgetting
 float compute_weight_linear(int new_event, float weight_last_event,
