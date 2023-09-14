ffab004bb9d5eb2807a35d55223ae9f1c7b3f192
uniooo <uniooo@uniooo.me>
2023-09-14 18:47:29 +0800

    DriverNodeSelector extends BaseSeedSelector; split snap_select and actorwise

--- network_diffusion/tpn/functions.py
+++ network_diffusion/tpn/functions.py
@@ -5,18 +5,19 @@ import networkx as nx
 from networkx.algorithms.approximation.dominating_set import min_weighted_dominating_set
 from networkx.algorithms.dominating import dominating_set
 
+from network_diffusion.mln.actor import MLNetworkActor
 from network_diffusion.mln.mlnetwork import MultilayerNetwork
 from network_diffusion.tpn.tpnetwork import TemporalNetwork
 
-def driver_nodes(net: MultilayerNetwork) -> List[int]:
+def driver_nodes(net: MultilayerNetwork) -> List[MLNetworkActor]:
     """Return driver nodes for a given network"""
 
     #TODO: multi-layer dominating set need, now only handle the first layer
     layer = net.layers['layer_1']   
     min_dominating_set = dominating_set(layer)
     # min_dominating_set = min_weighted_dominating_set(net)
-
-    return min_dominating_set
+    
+    return [net.get_actor(actor_id) for actor_id in min_dominating_set]
 
 
 
