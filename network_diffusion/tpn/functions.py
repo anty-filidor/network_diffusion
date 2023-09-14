3ccc7094fe7c8ea90c33cade8a2869b5296cf58b
uniooo <uniooo@uniooo.me>
2023-09-14 21:49:40 +0800

    multilayer network supported

--- network_diffusion/tpn/functions.py
+++ network_diffusion/tpn/functions.py
@@ -18,30 +18,36 @@ def driver_nodes(net: MultilayerNetwork) -> List[MLNetworkActor]:
     # min_dominating_set = min_weighted_dominating_set(net)
 
     min_dominating_set = []
-    for layer in net.layers.values():
-        min_dominating_set= minimum_dominating_set_with_initial(layer, min_dominating_set)
+    for layer in net.layers:
+        min_dominating_set= minimum_dominating_set_with_initial(net, layer, min_dominating_set)
 
     return [net.get_actor(actor_id) for actor_id in min_dominating_set]
 
 
-def minimum_dominating_set_with_initial(net: nx.Graph, initial_set: Set[int]) -> Set[int]:
+def minimum_dominating_set_with_initial(net: MultilayerNetwork, layer: str, initial_set: Set[int]) -> Set[int]:
     """
     Return a dominating set that includes the initial set.
-    G: nx.Graph
+    net: MultilayerNetwork
+    layer: layer name
     initial_set: set of nodes
     """
-    if not set(initial_set).issubset(set(net.nodes())):
-        raise ValueError("Initial set must be a subset of G's nodes")
+    actor_ids = [x.actor_id for x in net.get_actors()]
+    if not set(initial_set).issubset(set(actor_ids)):
+        raise ValueError("Initial set must be a subset of net's actors")
 
-    dominated = set(initial_set)
-    dominating_set = set(initial_set)
+    dominating_set = set(initial_set) 
+    
+    net_layer = net.layers[layer]
+    isolated = set(actor_ids) - set(net_layer.nodes())
+    dominating_set = dominating_set | isolated
+    dominated = dominating_set
 
     while len(dominated) < len(net):
         # Choose a node which dominates the maximum number of undominated nodes
-        u = max(net.nodes(), key=lambda x: len(set(net[x]) - dominated))
+        u = max(net_layer.nodes(), key=lambda x: len(set(net_layer[x]) - dominated))
         dominating_set.add(u)
         dominated.add(u)
-        dominated.update(net[u])
+        dominated.update(net_layer[u])
 
     return dominating_set
 
