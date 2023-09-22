5792ee3f44db0ba666bc20028c5377a3939702ff
Michał Czuba <michal.czuba.1995@gmail.com>
2023-09-22 15:13:03 +0200

    applied pre-commit

--- network_diffusion/tests/mln/test_mlnetwork.py
+++ network_diffusion/tests/mln/test_mlnetwork.py
@@ -12,7 +12,7 @@ import networkx as nx
 import numpy as np
 
 from network_diffusion import utils
-from network_diffusion.mln import MultilayerNetwork, MLNetworkActor
+from network_diffusion.mln import MLNetworkActor, MultilayerNetwork
 
 
 def copy_helper(original_network, copied_network):
@@ -118,11 +118,11 @@ class TestMultilayerNetwork(unittest.TestCase):
             ["marriage", "business"],
             "Incorrect layer names",
         )
-    
+
     def test_get_actor(self):
         self.assertEqual(
-            self.network.get_actor("Ridolfi"), 
-            MLNetworkActor("Ridolfi", {'marriage': None})
+            self.network.get_actor("Ridolfi"),
+            MLNetworkActor("Ridolfi", {"marriage": None}),
         )
 
     def test_copy(self):
