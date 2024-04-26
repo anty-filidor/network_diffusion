6ee768e483bb12ce0bd2073756119d76da8ca452
Michał Czuba <michal.czuba.1995@gmail.com>
2024-04-26 19:24:09 +0000

    extracted function to select seeds in CBIM

--- network_diffusion/mln/cbim.py
+++ network_diffusion/mln/cbim.py
@@ -1,3 +1,21 @@
+# Copyright 2024 by Michał Czuba, Piotr Bródka. All Rights Reserved.
+#
+# This file is part of Network Diffusion.
+#
+# Network Diffusion is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option) any
+# later version.
+#
+# Network Diffusion is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the  GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Network Diffusion. If not, see <http://www.gnu.org/licenses/>.
+# =============================================================================
+
 """Community based influence maximization algorithm."""
 
 import random
@@ -5,72 +23,9 @@ from typing import Any
 
 import networkx as nx
 import numpy as np
+import pandas as pd
 
-
-def compute_seed_quotas(
-    G: nx.Graph, communities: list[Any], num_seeds: int
-) -> list[int]:
-    """
-    Compute number of seeds to be chosen from communities according to budget.
-
-    We would like to select <num_seeds> from the network <G>. This function
-    will compute how many seeds take from each community basing on their sizes
-    compared to size of the <G>. There is a condition: communities should be
-    separable, i.e. each node must be in only one community.
-    """
-    if num_seeds > len(G.nodes):
-        raise ValueError("Number of seeds cannot be > number of nodes!")
-    quotas = []
-
-    # sort communities according to their sizes and remember their indices
-    # in the input matrix to return quotas in correct order
-    comms_sorting_order = sorted(
-        range(len(communities)),
-        key=lambda k: [len(c) for c in communities][k],
-        reverse=True,
-    )
-    comms_sorted = [communities[comm_idx] for comm_idx in comms_sorting_order]
-
-    # compute fractions of communities to be used as seeds
-    for communiy in comms_sorted:
-        quo = num_seeds * len(communiy) / len(G.nodes)
-        quotas.append(int(quo))
-    # print("raw quotas", quotas, "com-s", [len(c) for c in comms_sorted])
-
-    # quantisation of computed quotas according to strategy: first increase
-    # quotas in communities that were skipped and if there is no such community
-    # increase quotas in the largest communities that have capability to have
-    # quota enlarged; here we assume that community is at least 1 node large
-    while sum(quotas) < num_seeds:
-        if quotas[quotas.index(min(quotas))] == 0:
-            quotas[quotas.index(min(quotas))] += 1
-        else:
-            diffs = [len(com) - quo for quo, com in zip(quotas, comms_sorted)]
-            _quotas = quotas.copy()
-            while True:
-                quota_to_increase = max(_quotas)
-                if diffs[quotas.index(quota_to_increase)] > 0:
-                    break
-                del _quotas[_quotas.index(quota_to_increase)]
-            quotas[quotas.index(quota_to_increase)] += 1
-    # print("balanced quotas", quotas, "com-s", [len(c) for c in comms_sorted])
-
-    # reorder quotas for communities according to their initial order
-    quotas_desorted = [0] * len(communities)
-    for idx_sorted, idx_original in enumerate(comms_sorting_order):
-        quotas_desorted[idx_original] = quotas[idx_sorted]
-
-    # sanity check - the function is still in development mode
-    if sum(quotas) != num_seeds:
-        raise ArithmeticError("Error in the function!")
-    for quo, com in zip(quotas, comms_sorted):
-        if quo > len(com):
-            raise ArithmeticError("Error in the function!")
-    for quo, com in zip(quotas_desorted, communities):
-        if quo > len(com):
-            raise ArithmeticError("Error in the function!")
-
-    return quotas_desorted
+from network_diffusion.mln.kppshell import compute_seed_quotas
 
 
 def dsc(G: nx.graph, u: Any, v: Any) -> float:
@@ -284,56 +239,46 @@ def consolide_communities(
     return FC
 
 
-def get_katz_and_select_seeds(
-    final_communities: list[list[Any]], G: nx.Graph, seed_budget: int
-) -> list[Any]:
-    """Finding Katz centrality coeficient and seed node selection."""
-
-    # 1. Initialize the Seed node set (SN) and Community set (CS)
-    SN = np.zeros(len(final_communities))
-    CS = final_communities.copy()
-
-    # 2. Calculate Katz centrality for each node in each community
-    katzCentralityList = []
-    for C in CS:
-        katzCentralityDict = nx.katz_centrality(
-            G.subgraph(C), alpha=0.1, beta=1.0, max_iter=100, weight="weight"
+def _select_seeds_from_katz(kcl: list[list[dict[str, Any]]], quotas: list[int]):
+    """Select top-k nodes with highest katz-centrality from each community."""
+    seeds_ranked = []
+    for quota, decomposed_community in zip(quotas, kcl):
+        # sort all the nodes in each community based on Katz Centrality Coeficient in descending order
+        df = (
+            pd.DataFrame(decomposed_community)
+            .sort_values(["katz_centrality"], ascending=[False])
+            .reset_index(drop=True)
         )
-        katzCentralityList.append(
-            [(k, v) for k, v in katzCentralityDict.items()]
-        )
+        # select the quota number of highest Katz centrality coeficient nodes as seed nodes from each community (Ci)
+        seeds_ranked.extend(df.iloc[:quota]["node_id"].to_list())
+    return set(seeds_ranked)
 
-    # 3. Sort all the nodes in each community based on Katz Centrality Coeficient in descending order
-    for community in katzCentralityList:
-        community.sort(key=lambda a: a[1], reverse=True)
-    print(
-        "Katz Centrality coeficients for every node: "
-        + str(katzCentralityList)
-    )
 
-    # 4. Calculate required seed nodes from each community in quota based approach
-    quotaList = compute_seed_quotas(
-        G=G, communities=final_communities, num_seeds=seed_budget
-    )
-    print("Quotas for every community: " + str(quotaList))
+def get_katz_and_select_seeds(G: nx.Graph, communities: list[list[Any]], num_seeds: int) -> list[Any]:
+    """
+    Find Katz centrality coeficient and select seed nodes.
 
-    # 5. Select the quota number of highest Katz centrality coeficient nodes as seed nodes from each community (Ci)
-    SN = np.zeros(int(sum(quotaList)))
-    i = 0
-    j = 0
-    for C in CS:
-        for community in katzCentralityList:
-            for node in community:
-                if C.count(node[0]) > 0 and quotaList[i] > 0:
-                    quotaList[i] -= 1
-                    SN[j] = node[0]
-                    j += 1
-        i += 1
+    Implementation of Algorithm 3 from https://doi.org/10.1016/j.ins.2022.07.103
+    based on code from here: https://github.com/doublejv/CBIM-Implementation.
 
-    print("Community Set (CS): " + str(CS))
-    print("Seed Nodes: " + str(SN))
+    :param G: input graph
+    :param final_communities: nodes assigned to communities
+    :param seed_budget: how much nodes to choose
 
-    return SN
+    :return: list of picked nodes as seeds
+    """
+    katz_centralities = []
+
+    # calculate Katz centrality for each node in each community
+    for C in communities:
+        kc_raw = nx.katz_centrality(G.subgraph(C), alpha=0.1, beta=1.0, max_iter=100, weight="weight")
+        katz_centralities.append([{"node_id": k, "katz_centrality": v} for k, v in kc_raw.items()])
+
+    # calculate required seed nodes from each community in quota based approach
+    quotas = compute_seed_quotas(G=G, communities=communities, num_seeds=num_seeds)
+
+    # select seed set according to katz centality in each community
+    return _select_seeds_from_katz(kcl=katz_centralities, quotas=quotas)
 
 
 def cbim(
@@ -346,7 +291,7 @@ def cbim(
         mergingIndexThreshold=mergingIndexThreshold,
     )
     seed_set = get_katz_and_select_seeds(
-        final_communities=final_communities, G=net, seed_budget=seedNodeAmount
+        communities=final_communities, G=net, num_seeds=seedNodeAmount
     )
     return seed_set
 
@@ -374,19 +319,3 @@ def get_toy_network():
     net = nx.from_numpy_matrix(adjecency_matrix)
     net.remove_edges_from(nx.selfloop_edges(net))
     return net
-
-
-if __name__ == "__main__":
-    net = get_toy_network()
-    mergingIndexThreshold = 0.1
-    seedNodeAmount = 4
-    seeds = cbim(
-        net=net,
-        mergingIndexThreshold=mergingIndexThreshold,
-        seedNodeAmount=seedNodeAmount,
-    )
-    print(
-        f"nodes nb: {net.number_of_nodes()}, edges nb: {net.number_of_edges()}"
-    )
-    print(f"parameters: k: {seedNodeAmount}, δ: {mergingIndexThreshold}")
-    print(f"seeds: {seeds}")
