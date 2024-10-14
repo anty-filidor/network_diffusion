14d556a06bda78480fa1daa55f09b1a9f7e26edc
Michał Czuba <michal.czuba.1995@gmail.com>
2024-10-14 20:33:44 +0200

    added unittests for l2_course_net

--- network_diffusion/tpn/l2_course_net.py
+++ network_diffusion/tpn/l2_course_net.py
@@ -23,12 +23,14 @@ def get_l2_course_net(
     `"course"` (with edges obtained by filtering out a complete graph according
     to a monthly questionarre).
 
-    For details see a paper which analyses the network (constructed slightly
-    different - not as a multilayer graph): "Peer Interaction Dynamics and
-    Second Language Learning Trajectories During Study Abroad: A Longitudinal
-    Investigation Using Dynamic Computational Social Network Analysis" by
-    M.B. Paradowski, N. Whitby, M. Czuba, and P. Bródka in "Language Learning",
-    2024 (https://doi.org/10.1111/lang.12681).
+    For details see a paper which analyses the network: "Peer Interaction
+    Dynamics and Second Language Learning Trajectories During Study Abroad: A
+    Longitudinal Investigation Using Dynamic Computational Social Network
+    Analysis" by M.B. Paradowski, N. Whitby, M. Czuba, and P. Bródka in
+    "Language Learning", 2024 (https://doi.org/10.1111/lang.12681). Please note
+    that the networ used in paper was slightly different constructed: (1) it
+    was a singlelayer network, (2) nodes 11 and 17 were discarded due to
+    uncertainity of their data, (3) sources were not anonymised.
 
     :param node_features: wether to to load features of the nodes (mainly
         language profficiency and psychological metrics with personal data).
@@ -40,11 +42,10 @@ def get_l2_course_net(
 
     :return: a temporal, multilayer network
     """
-    s_path = Path("/Users/michal/Development/sna-arabic-course/networks")
+    s_path = Path(__file__).parent / "data"
     snap_1 = _read_snapshot(s_path, 1, node_features, edge_features, directed)
     snap_2 = _read_snapshot(s_path, 2, node_features, edge_features, directed)
     snap_3 = _read_snapshot(s_path, 3, node_features, edge_features, directed)
-    # TODO: change the path, remove students' names!
     return TemporalNetwork([snap_1, snap_2, snap_3])
 
 
@@ -99,7 +100,7 @@ def _read_node_attrs(csv_path: Path) -> dict[int, dict[str, Any]]:
 
 def _read_snapshot(
     src_path: Path,
-    snapshot_idx: int,
+    s_idx: int,
     node_features: bool,
     edge_features: bool,
     directed: bool,
@@ -108,9 +109,8 @@ def _read_snapshot(
     net_model = nx.DiGraph if directed else nx.Graph
 
     # read ego layer
-    eedges_df = _read_ego_edges(
-        src_path / f"ego_edges/{snapshot_idx}_ego_edges.csv", directed
-    )
+    eedges_path = src_path / f"{s_idx}_ego_edges.csv"
+    eedges_df = _read_ego_edges(eedges_path, directed)
     enet_attrs = list(set(eedges_df.columns).difference({"source", "target"}))
     enet = nx.from_pandas_edgelist(
         eedges_df,
@@ -119,9 +119,8 @@ def _read_snapshot(
     )
 
     # read course layer
-    cedges_df = _read_course_edges(
-        src_path / f"course_edges/{snapshot_idx}_course_edges.csv", directed
-    )
+    cedges_path = src_path / f"{s_idx}_course_edges.csv"
+    cedges_df = _read_course_edges(cedges_path, directed)
     cnet_attrs = list(set(cedges_df.columns).difference({"source", "target"}))
     cnet = nx.from_pandas_edgelist(
         cedges_df,
@@ -131,21 +130,28 @@ def _read_snapshot(
 
     # read nodes and their features
     if node_features:
-        nodes_attrs = _read_node_attrs(src_path / f"{snapshot_idx}_nodes.csv")
-        nx.set_node_attributes(cnet, nodes_attrs.copy())
-        nx.set_node_attributes(enet, nodes_attrs.copy())
+        features_dict = _read_node_attrs(src_path / f"{s_idx}_nodes.csv")
+        nx.set_node_attributes(cnet, features_dict.copy())
+        nx.set_node_attributes(enet, features_dict.copy())
+
+    # remove self loops from each layer
+    enet.remove_edges_from(nx.selfloop_edges(enet))
+    cnet.remove_edges_from(nx.selfloop_edges(cnet))
 
     # couple the layers and return the network
-    net = MultilayerNetwork(layers={"ego": enet, "course": cnet})
-    return net
+    return MultilayerNetwork(layers={"ego": enet, "course": cnet})
 
 
 if __name__ == "__main__":
     from network_diffusion.mln.functions import draw_mln
 
     l2_course_net = get_l2_course_net(
-        node_features=False, edge_features=False, directed=False
+        node_features=True, edge_features=False, directed=True
     )
+
     for snap in l2_course_net.snaps:
         print(snap)
         draw_mln(snap)
+
+
+# TODO: add sphinx docs
