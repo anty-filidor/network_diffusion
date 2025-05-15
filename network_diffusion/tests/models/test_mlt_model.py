e8e7d68ce7f808c61246399105037fb5c735fae9
Michał Czuba <michal.czuba.1995@gmail.com>
2025-05-15 20:48:38 +0200

    renamed fix_random_seed to set_rng_seed and madde it public

--- network_diffusion/tests/models/test_mlt_model.py
+++ network_diffusion/tests/models/test_mlt_model.py
@@ -4,7 +4,7 @@ from network_diffusion.models import MLTModel
 from network_diffusion.nets import get_toy_network_piotr
 from network_diffusion.seeding.mocking_selector import MockingActorSelector
 from network_diffusion.simulator import Simulator
-from network_diffusion.utils import fix_random_seed
+from network_diffusion.utils import set_rng_seed
 
 SPREADING_PARAMETERS = [
     {
@@ -75,7 +75,7 @@ class TestMLTModel(unittest.TestCase):
 
     def setUp(self) -> None:
 
-        fix_random_seed(42)
+        set_rng_seed(42)
 
         self.network = get_toy_network_piotr()
 
